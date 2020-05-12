625bf7bda52080ce077b7848c927ba52926b1cde
xcodebuild <me@xcodebuild.com>
2020-05-12 08:29:40 +0800

    add sitemap

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ router.all('*', async ctx => {
     if (ctx.url.endsWith('.png') || ctx.url.endsWith('.jpeg')) {
         ctx.redirect(`https://cdn.jsdelivr.net/gh/alibaba/lightproxy@gh-pages/${ctx.request.path}`);
         ctx.set('cache-control', 'public, max-age=604800, s-maxage=43200');
+    } else if (ctx.url === 'sitemap.xml') {
+        const res = await fetch('https://raw.githubusercontent.com/alibaba/lightproxy/gh-pages/sitemap.xml');
+        const text = await res.text();
+        ctx.set('cache-control', 's-maxage=1, stale-while-revalidate');
+        ctx.body = text;
     } else {
-        const res = await fetch('https://cdn.jsdelivr.net/gh/alibaba/lightproxy@gh-pages/index.html');
+        const res = await fetch('https://raw.githubusercontent.com/alibaba/lightproxy/gh-pages/index.html');
         const text = (await res.text()).replace(/(\/umi\..*?\.js|\/umi\..*?\.css)/g, 'https://cdn.jsdelivr.net/gh/alibaba/lightproxy@gh-pages/$1');
         ctx.set('cache-control', 's-maxage=1, stale-while-revalidate');
         ctx.body = text;
